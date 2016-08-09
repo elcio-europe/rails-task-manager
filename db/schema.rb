7a3d19d8c3cfa557fe5c5e7b3f9dc0eca4a3defc
Elcio <elcio@europe.com>
2016-08-09 11:54:44 -0300

    added false default value for done

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,9 @@ ActiveRecord::Schema.define(version: 20160809143810) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "content"
-    t.boolean  "done"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "done",       default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
